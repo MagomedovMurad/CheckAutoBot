f542c835aade72f4eb7a7bf375f05b9ee0bec32a
Мурад Магомедов <murad26.mag@gmail.com>
2020-02-15 14:22:19 +0300

    Добавлены проекты в решение

---
+++ CheckAutoBot/CaptchaSolver.Server/Controllers/CaptchaSolverController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CaptchaSolver.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CaptchaSolverController : ControllerBase
+    {
+        private readonly ILogger<CaptchaSolverController> _logger;
+
+        public CaptchaSolverController(ILogger<CaptchaSolverController> logger)
+        {
+            _logger = logger;
+        }
+
+        #region  SolverClient
+
+        [HttpGet("nexttask")]
+        public string Get()
+        {
+            HttpContext.Response.StatusCode = 403;
+            return null;
+          //  return "Hello";
+        }
+
+        [HttpPost("answer")]
+        public string Post()
+        {
+            return null;
+        }
+
+        #endregion
+
+        #region User
+
+
+
+        #endregion
+
+    }
+}
